b9ea59ca2fb8505cebf31087be8e5caa9d9b0c59
minhthieu213 <minhthieu213@gmail.com>
2025-01-07 22:04:57 +0700

    Init code story and featured mugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,31 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    letterSpacing:{
-      widest: '.25em'
-    },
     extend: {
+      letterSpacing:{
+        widestLg: '.25em'
+      },
       keyframes: {
         slideDown: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0)' },
+        },
+        fadeIn: {
+          from: {opacity: 0},
+          to: {opacity: 1}
         }
       },
       animation: {
         slideDown: 'slideDown .4s ease-in-out',
+        fadeIn: 'fadeIn .5s ease-in-out'
       },
       colors: {
-        'light-coffee': '#C89F94'
+        'coffee': {
+          50: '#E8D6D0',
+          200: '#C89F94',
+          400: '#A25F4B',
+          600: '#744838'
+        }
       }
     },
   },
